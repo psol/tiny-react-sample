065e63ce9c3fcc93921c3dab13f88c6126337f19
Benoît Marchal <github@psol.com>
2018-03-05 16:59:23 +0100

    Replace Object.assign() with object spread

--- src/store.js
+++ src/store.js
@@ -3,15 +3,15 @@ const CHANGE_FIELD = 'CHANGE_FIELD'
 export function Change (field, value) {
   return {
     type: CHANGE_FIELD,
-      field,
-      value
+    field,
+    value
   }
 }
 
 export function reducer (state, action) {
   switch (action.type) {
     case CHANGE_FIELD:
-      return { person: Object.assign({}, state.person, { [action.field]: action.value }) }
+      return { person: {...state.person, [action.field]: action.value } }
     default:
       return state
   }
