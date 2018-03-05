7384bbb20b5a4961b707cb43f597ffcec477bd6c
Benoît Marchal <github@psol.com>
2018-03-05 19:27:33 +0100

    Added REST sample

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { reducer } from './store'
 import Input from './Input'
 import Hello from './Hello'
+import GitHub from './GitHub'
 
-const store = createStore(reducer, { person: { first: '', last: '' } })
+const store = createStore(reducer, { person: { first: '', last: '' }, github: [] }, applyMiddleware(thunk))
 
 render(<Provider store={store}>
     <div>
       <Input />
       <Hello />
+      <GitHub />
     </div>
   </Provider>,
   document.getElementById('root'))
--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 const CHANGE_FIELD = 'CHANGE_FIELD'
+const RECEIVE_RESULTS = 'RECEIVE_RESULTS'
 
 export function Change (field, value) {
   return {
@@ -8,10 +9,33 @@ export function Change (field, value) {
   }
 }
 
+function receiveResults (results) {
+  return {
+    type: RECEIVE_RESULTS,
+    query: results[0],
+    results: results
+  }
+}
+
+export function Search () {
+  return function (dispatch, getState) {
+    const person = getState().person
+    const query = (person.first + ' ' + person.last).trim()
+    if(query.length > 0) {
+      return fetch(encodeURI(`https://api.github.com/search/users?q=${query}`))
+        .then(response => response.json())
+        .then(json => dispatch(receiveResults(json)))
+    }
+    else return Promise.resolve()
+  }
+}
+
 export function reducer (state, action) {
   switch (action.type) {
     case CHANGE_FIELD:
-      return { person: {...state.person, [action.field]: action.value } }
+      return { person: {...state.person, [action.field]: action.value }, github: state.github }
+    case RECEIVE_RESULTS:
+      return { person: state.person, github: action.results.items }
     default:
       return state
   }
